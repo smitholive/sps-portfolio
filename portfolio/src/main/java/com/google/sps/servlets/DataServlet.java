77a4854b7d04966416b4bf779d397eefe78cd3d7
Olivia Smith <osmith@sps-program.com>
2020-06-15 16:55:30 -0500

    Added Datastore functionality; migrated to Contact page and added name / timestamp fields

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,16 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import java.util.Date; 
+import java.util.ArrayList;
+import java.util.List;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,23 +34,51 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private final ArrayList<String> messages;
-
   public DataServlet(){
-      messages = new ArrayList<String>();
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+     Query query = new Query("Entry").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<String> names = new ArrayList<String>();
+    ArrayList<String> messages = new ArrayList<String>();
+    ArrayList<Date> timestamps = new ArrayList<Date>();
+
+    for (Entity entity : results.asIterable()) {
+      String name = (String) entity.getProperty("name");
+      String message = (String) entity.getProperty("message");
+      Date timestamp = new Date((long) entity.getProperty("timestamp"));
+      names.add(name);
+      messages.add(message);
+      timestamps.add(timestamp);
+    }
     response.setContentType("text/html;");
-    response.getWriter().println("Hello Olivia!");
+    for(int i = 0; i < messages.size(); i++){
+        response.getWriter().printf("%s at %s said: ", names.get(i), timestamps.get(i).toString());
+        response.getWriter().println(messages.get(i));
+    }
   }
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String message = request.getParameter("message-input");
-    this.messages.add(message);
+    String name = request.getParameter("name-input");
+    // get date & time of post request
+    Date date = new Date(); 
+
+    Entity entryEntity = new Entity("Entry");
+    entryEntity.setProperty("timestamp", date.getTime());
+    entryEntity.setProperty("message", message);
+    entryEntity.setProperty("name", name);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(entryEntity);
+
     // note: this URL will have to change once site is deployed
-    response.sendRedirect("https://8080-dot-12685145-dot-devshell.appspot.com/index.html");
+    response.sendRedirect("https://8080-dot-12685145-dot-devshell.appspot.com/contact.html");
   }
 }
