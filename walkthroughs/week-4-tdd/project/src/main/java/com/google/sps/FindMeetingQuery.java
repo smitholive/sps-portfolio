5d2897d56fa7fdfd0817cb6296597ae0605004ce
Olivia Smith <osmith@sps-program.com>
2020-07-01 10:54:10 -0500

    all tests passing, style fixes

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -24,68 +24,56 @@ import java.util.Set;
 
 public final class FindMeetingQuery {
   private boolean intersects(Collection<String> requestAttendees, Set<String> eventAttendees) {
-      // a function that tests if the attendees of an event intersect with the request's
-      // attendees. On^2
-     for(String eventAttendee : eventAttendees) {
-        for(String requestAttendee : requestAttendees) {
-            if(requestAttendee.equals(eventAttendee)) {
-                return true;
-            }
-        }
-     }
-     return false;
-  }
-  private Collection<Event> filterAttendeesIntersect(Collection<Event> events, MeetingRequest request) {
-      // a function that takes a collection of events and a meeting request
-      // it returns a collection of events where the events' attendees intersect with
-      // the attendees of the meeting request
-      Collection<Event> filtered = new ArrayList<Event>();
-      Collection<String> requestAttendees = request.getAttendees();
-
-      for(Event event : events) {
-        Set<String> eventAttendees = event.getAttendees();
-        if(intersects(requestAttendees, eventAttendees)) {
-            filtered.add(event);
+      // a function that tests if the attendees of an event intersect with request's attendees
+    for(String eventAttendee : eventAttendees) {
+      for(String requestAttendee : requestAttendees) {
+        if(requestAttendee.equals(eventAttendee)) {
+            return true;
         }
       }
-      return filtered;
+    }
+    return false;
+  }
+  private Collection<Event> filterAttendeesIntersect(Collection<Event> events, MeetingRequest request) {
+    // a function that takes a collection of events and a meeting request
+    // it returns a collection of events where the events' attendees intersect with
+    // the attendees of the meeting request
+    Collection<Event> filtered = new ArrayList<Event>();
+    Collection<String> requestAttendees = request.getAttendees();
+    for(Event event : events) {
+      Set<String> eventAttendees = event.getAttendees();
+      if(intersects(requestAttendees, eventAttendees)) {
+        filtered.add(event);
+      }
+    }
+    return filtered;
   }
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     // a method that returns a collection of timeranges where people are free to meet
     // event: a period of time where people are busy
     // "events" contains data that referring to when people are busy
-
-    // algorithm works as follows:
-    // check null cases & request validity
-    // filter events s.t. we maintain events that pertain to the attendees in
-    //   the request
-    // sort events by start time
-    // check start of day edge case
-    // for every event:
-    //   check if c
-    //   if end time < start time of next event
-    // check end of day edge case
-    // create a time range for the end time and add it to the return value
-    // return the collection of time ranges return value
-
-    if(events == null || request == null) { // null case check
-        throw new UnsupportedOperationException("Null Parameter 'events' or 'reqest'.");
+    
+    // null case check
+    if(events == null || request == null) {
+        throw new UnsupportedOperationException("Null Parameter 'events' or 'request'.");
     }
     // initialize return value
     Collection<TimeRange> openings = new ArrayList<TimeRange>();
 
-    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) { // edge case: request longer than a day
+    // edge case: request longer than a day
+    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
         return openings;  // return empty collection
     }
     // filter out events that do not pertain to the people in our meeting request
     Collection<Event> filteredEvents = filterAttendeesIntersect(events, request);
     
-    if(filteredEvents.isEmpty()){ // edge case: events is empty; all day is open
+    // edge case: filtered events is empty; all day is open
+    if(filteredEvents.isEmpty()) {
       openings.add(TimeRange.WHOLE_DAY);
       return openings;
     }
 
-    // Extract busy TimeRanges
+    // extract busy TimeRanges
     ArrayList<TimeRange> eventTimes = new ArrayList<TimeRange>();
     for(Event event : filteredEvents) {
         eventTimes.add(event.getWhen());
@@ -106,27 +94,30 @@ public final class FindMeetingQuery {
     // for every TimeRange
     while(timeIterator.hasNext()) {
       // retrieve the start time of next TimeRange 
-      // (recall that they are sorted and that Collections does not store duplicates)
       temp2 = timeIterator.next();
 
+      // compute end times of previous & next TimeRanges
       int endTime1 = temp1.start() + temp1.duration();
       int endTime2 = temp2.start() + temp2.duration();
-      
+
+      // if the events do not overlap and the previous event ends before the next event
       if(!temp1.overlaps(temp2) && endTime1 < endTime2) {
         int gap = temp2.start() - endTime1;
+        // if the gap between events is sufficient in length for request
         if(gap >= request.getDuration()) {
           openings.add(TimeRange.fromStartEnd(temp1.start() + temp1.duration(), temp2.start(), false));
         }
       }
-      temp1 = TimeRange.fromStartDuration(temp2.start(), temp2.duration());
+      // update previous event if it ends before the next referenced event
+      // allows us to accomodate for nested cases
+      if(endTime1 < endTime2) {
+        temp1 = TimeRange.fromStartDuration(temp2.start(), temp2.duration());
+      }
     }
-    
     // edge case: end of day
     if(temp1.start() + temp1.duration() < TimeRange.END_OF_DAY) {
-      //int startTime = temp.start() + temp.duration();
       openings.add(TimeRange.fromStartEnd(temp1.start() + temp1.duration(), TimeRange.END_OF_DAY, true));
     }
-
-    return openings; // question: do we want to return empty if no openings exist?
+    return openings;
   }
 }
